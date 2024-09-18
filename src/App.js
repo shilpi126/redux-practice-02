cfb7141036b5ce20d1cfed92ae22c3d9f03ebf00
shilpi <shilpi8624@gmail.com>
2024-09-18 23:14:24 +0530

    :bow_and_arrow: save cart data to database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import CartModal from './components/CartModal';
 import Header from './components/Header';
 import Product from './components/Product';
 import store from './store';
-import {Provider} from "react-redux"
+import {Provider, useSelector} from "react-redux"
 
 function App() {
+  const cart = useSelector((state)=> state.cart.cartItems)
+  //console.log(cart)
+
+
+  useEffect(()=>{
+    fetch("https://redux-pactice-default-rtdb.firebaseio.com/cart.json",{
+      method: 'PUT',
+      body:JSON.stringify(cart)
+    })
+  },[cart])
+
+
+
   return (
-    <Provider store={store}>
+      <React.Fragment>
       <Header/>
      <div className='main'>
      <CartModal/>
      <Product/>
      </div>
-    </Provider >
+     </React.Fragment>
+    
   );
 }
 
